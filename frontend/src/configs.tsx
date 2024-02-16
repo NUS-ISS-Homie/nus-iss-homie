804815a19f13418fb44b9e2984678482eae14ec6
erinmayg <erinmaygunawan@gmail.com>
2024-02-16 16:28:12 +0800

    Setup socket context

---
+++ frontend/src/configs.tsx
@@ -0,0 +1,4 @@
+const URI_HOME_SVC = 'http://localhost:8000';
+const PREFIX_HOME_SVC = '';
+
+export const URL_HOME_SVC = URI_HOME_SVC + PREFIX_HOME_SVC;
---
+++ frontend/src/context/Sockets.ts
@@ -0,0 +1,10 @@
+import { io } from 'socket.io-client';
+import {
+  URL_HOME_SVC,
+} from '../configs';
+
+const sockets = {
+  homeSocket: io(URL_HOME_SVC),
+};
+
+export default sockets;
