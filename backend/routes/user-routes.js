d20d530dd2db767b248d325f0f3d2311d06d584d
erinmayg <erinmaygunawan@gmail.com>
2024-02-22 18:03:40 +0800

    Remove unused imports

--- backend/routes/user-routes.js
+++ backend/routes/user-routes.js
@@ -1,12 +1,4 @@
 import express from 'express';
-import cors from 'cors';
-
-const app = express();
-
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-app.use(cors()); // config cors so that front-end can use
-app.options('*', cors());
 import {
   changePassword,
   changeUsername,
