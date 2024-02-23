2052ef1d35749e02a20508e0ac2df05972fdf23c
erinmayg <60438326+erinmayg@users.noreply.github.com>
2024-02-23 23:44:55 +0800

    HOMIE7-7 User Service CRUD Backend (#4)
    * Add user controllers, models, routes
    
    * Add User CRUD
    
    * Add unit tests for user related APIs
    
    ---------
    
    Co-authored-by: Florencia Martina <florenciamartina15@gmail.com>

--- backend/index.js
+++ backend/index.js
@@ -1,32 +1,25 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config';
 
 const app = express();
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors()); // config cors so that front-end can use
 app.options('*', cors());
 
+import userRoutes from './routes/user-routes.js';
+
+// const userRouter = express.Router();
 const router = express.Router();
 
 // Controller will contain all the User-defined Routes
-router.get('/', (_, res) => res.send('Hello World from Homie!'));
+router.get('/', (_, res) => res.send('Hello World from Homie'));
 
-const port = process.env.PORT;
-
-// app.get('/', (req, res) => {
-//     res.send('Hello World!');
-// });
-
-app.use('/', router).all((_, res) => {
+app.use('/api/user', userRoutes).all((_, res) => {
   res.setHeader('content-type', 'application/json');
   res.setHeader('Access-Control-Allow-Origin', '*');
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+app.listen(8000, () => console.log('Homie listening on port 8000'));
 
 export default app;
---
+++ backend/routes/user-routes.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import {
+  changePassword,
+  changeUsername,
+  createUser,
+  deleteUser,
+  signIn,
+} from '../controllers/user-controller.js';
+
+const router = express.Router();
+
+// Controller will contain all the User-defined Routes
+router.get('/', (_, res) => res.send('Hello World from user-service'));
+
+router.post('/signup', createUser);
+router.get('/login', signIn);
+router.put('/change-password', changePassword);
+router.put('/change-username', changeUsername);
+router.delete('/delete-user', deleteUser);
+
+export default router;
