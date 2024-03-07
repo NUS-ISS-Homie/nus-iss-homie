781db02af7e303e3dc0a23de4b8ddbbaaa539000
erinmayg <erinmaygunawan@gmail.com>
2024-03-07 16:27:38 +0800

    Make admin singular

--- backend/models/home/home-model.js
+++ backend/models/home/home-model.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 var Schema = mongoose.Schema;
 let HomeModelSchema = new Schema({
-  adminUser: { type: [Schema.Types.String], required: true, unique: true },
+  adminUser: { type: Schema.Types.String, required: true, unique: true },
   users: { type: [Schema.Types.String] },
 });
 
