56d91a8e0ed107e83a4689001e5dde767129e758
Dylan Goh <dylan.goh@ncs.com.sg>
2024-03-21 10:55:20 +0800

    Added backend sync.

--- frontend/src/context/GroceryItemContext.tsx
+++ frontend/src/context/GroceryItemContext.tsx
@@ -1,7 +1,21 @@
+import React, { createContext, useState, useContext, useEffect } from 'react';
 import { useSockets } from './SocketContext';
 import { useUser } from './UserContext';
+
+interface IGroceryContext {}
+
+const GroceryContext = createContext<IGroceryContext>({});
 
 export function GroceryProvider({ children }: { children: React.ReactNode }) {
   const { user_id } = useUser();
   const { createGroceryItem, homeSocket } = useSockets();
+  const [loading, setLoading] = useState(false);
+
+  const createGrocery = () => {
+    setLoading(true);
+  };
+
+  return (
+    <GroceryContext.Provider value={{}}>{children}</GroceryContext.Provider>
+  );
 }
