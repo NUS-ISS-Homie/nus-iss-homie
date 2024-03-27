0b87dfd406182fe33ed99fc7fe8f821ff5b9a445
erinmayg <erinmaygunawan@gmail.com>
2024-03-27 16:13:52 +0800

    Update Expenses frontend

---
+++ frontend/src/utils/api-expense.ts
@@ -0,0 +1,43 @@
+import {
+  Expense,
+  ExpenseResponse,
+  ExpensesResponse,
+  NewExpense,
+  UpdateExpense,
+} from '../@types/Expense';
+import { URL_EXPENSE_SVC } from '../configs';
+import { API, requests } from './api-request';
+
+const APIExpense = {
+  createExpense: (
+    expense: NewExpense
+  ): Promise<API.Response<ExpenseResponse>> => {
+    return requests.post(URL_EXPENSE_SVC, '', expense);
+  },
+
+  getExpense: (expenseId: string): Promise<API.Response<ExpenseResponse>> => {
+    return requests.get(URL_EXPENSE_SVC, `/${expenseId}`);
+  },
+
+  getExpenses: (homeId: string): Promise<API.Response<ExpensesResponse>> => {
+    return requests.put(URL_EXPENSE_SVC, '', { home: homeId });
+  },
+
+  updateExpense: (
+    updatedExpense: UpdateExpense
+  ): Promise<API.Response<ExpenseResponse>> => {
+    return requests.put(
+      URL_EXPENSE_SVC,
+      `/${updatedExpense._id}`,
+      updatedExpense
+    );
+  },
+
+  deleteExpense: (
+    expenseId: string
+  ): Promise<API.Response<ExpenseResponse>> => {
+    return requests.delete(URL_EXPENSE_SVC, `/${expenseId}`, {});
+  },
+};
+
+export default APIExpense;
